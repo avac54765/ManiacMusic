c8fe267feedf92cb349db0b5f5fa35cadd0d1441
alexac54767 <alexarosecarlson@icloud.com>
2023-05-22 21:52:57 -0700

    fixing redirect var to my page

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var redirect_uri = "https://makeratplay.github.io/SpotifyWebAPI/"; // change this your value
+var redirect_uri = "https://avac54765.github.io/ManiacMusic/"; // change this your value
 //var redirect_uri = "http://127.0.0.1:5500/index.html";
  
 
